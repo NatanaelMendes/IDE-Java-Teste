e72742971ad5acc052ff39b3c45f0a5d617b1304
NatanaelMendes <natanael.mendesalves@gmail.com>
2022-01-27 14:21:49 -0300

    Atualização com return no exercicio dos quadrilateros

---
+++ src/br/com/dio/ex03/Main.java
@@ -0,0 +1,14 @@
+package br.com.dio.ex03;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Exercício quadrilátero");
+		System.out.println(Quadrilatero.area(3));
+		System.out.println(Quadrilatero.area(5d, 5d));
+		System.out.println(Quadrilatero.area(7, 8, 9));
+		System.out.println(Quadrilatero.area(5f, 5f));
+
+	}
+}
