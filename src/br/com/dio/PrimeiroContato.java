633d0b37509f11cc8e9af29c0923b02208670c11
NatanaelMendes <natanael.mendesalves@gmail.com>
2022-01-27 13:29:33 -0300

    Atualizando tipos variaveis

---
+++ src/br/com/dio/PrimeiroContato.java
@@ -0,0 +1,63 @@
+package br.com.dio;
+
+public class PrimeiroContato {
+
+	public static void main(String[] args) {
+
+		byte b1 = 10;
+		byte b2 = 20;
+
+		short s1 = 2000;
+		short s2 = 4000;
+
+		int i1 = 200000000;
+		int i2 = 55555555;
+
+		long l1 = 1000000000000000L;
+		long l2 = 200000000000000L;
+
+		float f1 = 10.5F;
+		float f2 = 4.5F;
+
+		double d1 = 85.3;
+		double d2 = 99.840D;
+
+		char c1 = 'W';
+		char c2 = '\u0057';
+
+		String st1 = "Fulano";
+		String st2 = "Cicrano";
+		String st3 = "iajks aksld jsoakdlkasd";
+		
+		String dt1 = "27/01/2022";
+		
+		boolean bo1 = true;
+		boolean bo2 = false;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(l1);
+		System.out.println(l2);
+		System.out.println(f1);
+		System.out.println(f2);
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(st1);
+		System.out.println(st2);
+		System.out.println(st3);
+		System.out.println(d1);
+		System.out.println(bo1);
+		System.out.println(bo2);
+
+
+		
+		
+	}
+
+}
